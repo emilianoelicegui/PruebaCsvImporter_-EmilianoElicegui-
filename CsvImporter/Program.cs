8f793782f116632216622e17b944a0846fb91609
Emiliano Elicegui <emielicegui@gmail.com>
2021-03-27 15:45:21 -0300

    add SqlBulkCopy for inserts

--- CsvImporter/Program.cs
+++ CsvImporter/Program.cs
@@ -1,27 +1,26 @@
 ﻿using AutoMapper;
 using CsvImporter.Helpers;
-using EFCore.BulkExtensions;
 using FileHelpers;
 using Models;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading;
+using Microsoft.EntityFrameworkCore;
+using CsvImporter.Helpers.ObjectDataReader;
+using Microsoft.Data.SqlClient;
 
 namespace CsvImporter
 {
     public class Program
     {
         public static bool Exited;
-        public static long StartingMb;
-        public static long PointsCount;
         public static DateTime TestStart;
         public static int ProcessedPoints;
 
         static void Main(string[] args)
         {
-            //var memoryLogger = new Thread(Logging); memoryLogger.Start();
+            var memoryLogger = new Thread(Logging); memoryLogger.Start();
 
             ImporterFileHelper();
         }
@@ -41,17 +40,60 @@ namespace CsvImporter
 
             try
             {
+
                 using (var context = new DBContext())
                 {
-                    context.BulkInsert(items);
-                    context.SaveChanges();
+                    context.Database.BeginTransaction();
+
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(context.Database.GetDbConnection().ConnectionString))
+                    {
+                        bulkCopy.DestinationTableName = "dbo.Items";
+
+                        try
+                        {
+                            // Write from the source to the destination.
+                            var reader = items.AsDataReader();
+
+                            SqlBulkCopyColumnMapping PointOfSale =
+                            new SqlBulkCopyColumnMapping("PointOfSale", "PointOfSale");
+                            bulkCopy.ColumnMappings.Add(PointOfSale);
+
+                            SqlBulkCopyColumnMapping Product =
+                            new SqlBulkCopyColumnMapping("Product", "Product");
+                            bulkCopy.ColumnMappings.Add(Product);
+
+                            SqlBulkCopyColumnMapping Date =
+                            new SqlBulkCopyColumnMapping("Date", "Date");
+                            bulkCopy.ColumnMappings.Add(Date);
+
+                            SqlBulkCopyColumnMapping Stock =
+                            new SqlBulkCopyColumnMapping("Stock", "Stock");
+                            bulkCopy.ColumnMappings.Add(Stock);
+
+                            bulkCopy.BulkCopyTimeout = 0;
+
+                            bulkCopy.WriteToServer(reader);
+
+                            context.Database.CommitTransaction();
+
+                            Console.WriteLine($"Saving list ({items.Count()}) items correct");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            context.Database.RollbackTransaction();
+                        }
+                    }
                 }
 
-                Console.WriteLine($"Saving list ({items.Count()}) items correct");
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error saving items, details: {ex.Message}");
+            }
+            finally
+            {
+                Exited = true;
             }
         }
 
@@ -60,23 +102,13 @@ namespace CsvImporter
             var lastProcessedPoints = 0;
             while (!Exited)
             {
-                Thread.Sleep(1000);
+                Thread.Sleep(60 * 1000);
                 var lapsed = (DateTime.Now - TestStart);
                 var currentUsage = (GC.GetTotalMemory(false) / 1024 / 1024);
                 var processedDelta = Math.Round((ProcessedPoints - lastProcessedPoints) * 2.0 / 1000);
                 lastProcessedPoints = ProcessedPoints;
 
                 Console.WriteLine("Time Elapsed {0}, Memory Used: {1}Mb, Aprox KPoints/Sec: {2}", lapsed, currentUsage, processedDelta);
-
-                //if (currentUsage > StartingMb * 1.1)
-                //{
-                //    throw new Exception("Test1.Main(): TEST FAILED: EXCESS MEMORY USAGE");
-                //}
-
-                //if ((PointsCount / lapsed.TotalMilliseconds) < 100)
-                //{
-                //    throw new Exception("Test1.Main(): TEST FAILED: TOO SLOW");
-                //}
             }
         }
     }
